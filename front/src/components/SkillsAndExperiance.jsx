830a1151ea4e97f3b994249065d294ccd22de5a1
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2024-11-17 17:33:59 +0100

    Update SkillsAndExperiance.jsx

--- front/src/components/SkillsAndExperiance.jsx
+++ front/src/components/SkillsAndExperiance.jsx
@@ -2,12 +2,12 @@ import { FaCss3Alt, FaDocker, FaGitSquare, FaHtml5, FaPython, FaReact } from "re
 
 import { IoLogoJavascript } from "react-icons/io5";
 import { BsFiletypeJson } from "react-icons/bs";
-import { SiExpress, SiFlask, SiMongodb, SiMysql, SiPhp, SiTailwindcss } from "react-icons/si";
+import { SiExpress, SiFlask, SiMongodb, SiMysql, SiPhp, SiTailwindcss, SiTypescript } from "react-icons/si";
 import { DiDjango, DiNodejs } from "react-icons/di";
 
 export default function SkillsAndExperiance() {
   return (
-    <div style={{width:'90%', paddingInline:'1vw', paddingTop:'1vw', paddingBottom:'1vw'}} className='shadow-2xl rounded-lg self-center'>
+    <div style={{width:'90%', paddingInline:'1vw', paddingTop:'1vw', paddingBottom:'1vw'}} className='shadow-2xl bg-[#393838] rounded-lg self-center'>
                 <h1 style={{ paddingBottom:'1vw'}} className='text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl'>Skills</h1>
                 <div className='flex flex-wrap gap-[5vw] items-center justify-center md:gap-[3vw] md:justify-start'>
                     <span className='flex bg-dark rounded-lg items-center' style={{gap:'0.3vw', padding:'0.2vw'}}>
@@ -25,6 +25,10 @@ export default function SkillsAndExperiance() {
                     <span className='flex bg-dark rounded-lg items-center' style={{gap:'0.3vw', padding:'0.2vw'}}>
                        <FaReact color='#00b7eb' className='text-5xl sm:text-6xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl'/> 
                        <p className='hidden md:block text-lg sm:text-xl md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl'>React.js</p>
+                    </span>
+                    <span className='flex bg-dark rounded-lg items-center' style={{gap:'0.3vw', padding:'0.2vw'}}>
+                       <SiTypescript color='#00b7eb' className='text-5xl sm:text-6xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl'/> 
+                       <p className='hidden md:block text-lg sm:text-xl md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl'>Typescript</p>
                     </span>
                     <span className='flex bg-dark rounded-lg items-center' style={{gap:'0.3vw', padding:'0.2vw'}}>
                         <img className='w-[8vw] sm:w-[6vw]' src='images/asp.png'/>
@@ -86,3 +90,4 @@ export default function SkillsAndExperiance() {
             </div>
   )
 }
+
